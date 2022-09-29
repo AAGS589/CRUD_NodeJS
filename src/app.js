8e58da03ae0bfe229b63c8f948c18bfaffe73e30
AAGS589 <alonsogozsamchez@gmail.com>
2022-09-28 19:45:02 -0500

    ultimas configuraciones

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
+const cors = require ('cors');
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 // middlewares
 app.use(morgan('dev'));
 app.use(myConnection(mysql, {
-    host: '34.229.155.177',
+    host: '54.89.203.179',
     user: 'ubuntu',
     password: 'password',
     port: 3306,
@@ -26,6 +27,7 @@ app.use(myConnection(mysql, {
 app.use(express.urlencoded({extended: false}))
 // routes
 app.use('/', customerRoutes);
+app.use(cors())
 
 // statics files
 app.use(express.static(path.join(__dirname, 'public')));
